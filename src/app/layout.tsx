0150f040cbd3913c5ab79d99e1c60e4a7ac58e3d
Amitava Datta <dattaarup2004@gmail.com>
2025-09-24 10:39:38 +0000

    Also this

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
       </head>
       <body className="font-body antialiased">
         <AuthProvider>
-          <div className="relative flex min-h-screen w-full flex-col">
+          <div className="flex min-h-screen w-full flex-col">
             <Navbar />
             <main className="flex-1">{children}</main>
           </div>
