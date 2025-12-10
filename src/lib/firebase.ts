63f858be27297ba5cf3ac57555a8785c54a4f875
Pranay De <pranayde201@gmail.com>
2025-12-10 09:17:31 +0000

    Put the firebase config details in env

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,13 +8,13 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBWQhEjTAeBQDTuNYmflqWB6kjNyR4_v5U",
-  authDomain: "civicpulse-9fe2f.firebaseapp.com",
-  projectId: "civicpulse-9fe2f",
-  storageBucket: "civicpulse-9fe2f.firebasestorage.app",
-  messagingSenderId: "432002831184",
-  appId: "1:432002831184:web:2010ec55f4e72eef1e9255",
-  measurementId: "G-2Y3KDKT3YX"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
